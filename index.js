c7cbba28d11aac21b35f77603e1d05d026cfa064
Daniel Otabor <danieldamianotabor@gmail.com>
2022-05-22 23:52:18 +0100

    adding files

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App setup
+var app = express();
+var server = app.listen(4000, function() {
+    console.log('listening to requests on port 4000');
+});
+
+//Static files
+app.use(express.static('public'));
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', function(){
+    console.log('made socket connection');
+});
