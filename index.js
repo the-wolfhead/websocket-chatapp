9ff66edcffa2dfe2dee00ae2723247263f3e58a5
Daniel Otabor <danieldamianotabor@gmail.com>
2022-05-23 09:32:30 +0100

    inputing chat functionality

--- index.js
+++ index.js
@@ -15,4 +15,8 @@ var io = socket(server);
 
 io.on('connection', function(socket){
     console.log('Made socket connection', socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    })
 });
